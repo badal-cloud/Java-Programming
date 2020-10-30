7ab84c28a0d01d670b0d32f541b1bcb193007c6e
badal-cloud <aryar6563@gmail.com>
2020-10-30 11:18:32 -0700

    sum of three no. at run time argument

---
+++ P3 Sum of 3 no. run time/sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class sum
+{ 	public static void main(String[] rdf)
+	{	Scanner sc=new Scanner(System.in);
+		System.out.println("Github username: badal-cloud");
+		System.out.println("Enter 1st Number");
+		int a=sc.nextInt();
+		System.out.println("Enter 2nd Number");
+		int b=sc.nextInt();
+		System.out.println("Enter 3rd Number");
+		int c=sc.nextInt();
+		int d=a+b+c;
+		System.out.println("Sum of the Numbers ="+d);
+	}	}
+
